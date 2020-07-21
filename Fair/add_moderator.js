3edf6589ac3b103af3d1a0536e1502f85e74de00
Manas Bhardwaj <mb.manasbhardwaj@gmail.com>
2020-07-21 18:48:11 +0530

    moderator addition code completed and tested

--- Fair/add_moderator.js
+++ Fair/add_moderator.js
@@ -9,8 +9,8 @@ let cFile = process.argv[2];
 let userToAdd = process.argv[3];
 
 //function banate hi usko call kar diya
-(async function () {
-    try {
+(async function(){
+    try{
         await loginHelper();
         //******************************Home page********************************
         let dropdown = await driver.findElement(swd.By.css("a[data-analytics=NavBarProfileDropDown]"));
@@ -18,6 +18,7 @@ let userToAdd = process.argv[3];
         let adminBtn = await driver.findElement(swd.By.css("a[data-analytics=NavBarProfileDropDownAdministration]"))
         await adminBtn.click();
         console.log("Admin page reached");
+        
         //******************************Manage challenges*************************
         await waitForLoader();
         let lis = await driver.findElements(swd.By.css(".administration header ul li"));
@@ -28,28 +29,26 @@ let userToAdd = process.argv[3];
         // one by one visit every question and add userToAdd as moderator
         let qIdx = 0;
         let qElement = await getQuestionElement(qIdx, managePageURL);
-        while (qElement !== null) {
+        while(qElement !== null){
             await handleQuestion(qElement);
             qIdx++;
             qElement = await getQuestionElement(qIdx, managePageURL);
         }
-
     }
-    catch (err) {
+    catch(err){
         console.log(err);
     }
 })();
 
-async function loginHelper() {
+async function loginHelper(){
     //selenium inbuilt
     await driver.manage().setTimeouts({
-        implicit: 10000,
-        pageLoad: 10000,
+        implicit:10000,
+        pageLoad:10000,
     })
     // buffer credentials
     let bCredentials = await fs.promises.readFile(cFile);
     let myCredentials = JSON.parse(bCredentials);
-    // console.log(myCredentials);
     let user = myCredentials.user;
     let pwd = myCredentials.password;
     let url = myCredentials.url;
@@ -70,15 +69,55 @@ async function loginHelper() {
     console.log("User logged in");
 }
 
-async function waitForLoader() {
+async function waitForLoader(){
     let loader = await driver.findElement(swd.By.css("#ajax-msg"));
     await driver.wait(swd.until.elementIsNotVisible(loader));
 }
 
-async function handleQuestion(qElement){
+async function getQuestionElement(qIdx, managePageURL){
+    await driver.get(managePageURL);
+    let pIdx = parseInt(qIdx / 10);     //pageIndex
+    let pQueIdx = qIdx % 10;            //pageQuestionIndex
+    console.log(pIdx + " " + pQueIdx);
     
+    let paginationBtns = await driver.findElements(swd.By.css(".pagination li"));
+    let nextPageBtn = paginationBtns[paginationBtns.length - 2];
+    let classOnNextPageBtn = await nextPageBtn.getAttribute("class");
+    for(let i=0; i<pIdx; i++){
+        if(classOnNextPageBtn !== "disabled"){
+            await nextPageBtn.click();
+            paginationBtns = await driver.findElements(swd.By.css(".pagination li"));
+            nextPageBtn = paginationBtns[paginationBtns.length - 2];
+            classOnNextPageBtn = await nextPageBtn.getAttribute("class");
+        }
+        else{
+            return null;
+        }
+    }
+    // console.log(pQueIdx);
+    let questionsElements = await driver.findElements(swd.By.css(".backbone.block-center"));
+    // console.log(questionsElements.length);
+    if(pQueIdx < questionsElements.length){
+        return questionsElements[pQueIdx];
+    }
+    else{
+        return null;
+    }
 }
 
-async function getQuestionElement(qIdx, managePageURL){
-    
+async function handleQuestion(qElement){
+    let qUrl = await qElement.getAttribute("href");
+    await driver.get(qUrl);
+    await waitForLoader();
+    //wait
+    // await driver.wait(swd.until.elementLocated(swd.By.css("span.tag")));
+    // await driver.wait(swd.until.elementLocated(swd.By.css(".error.tags.span12")));
+    await driver.wait(swd.until.elementLocated(swd.By.css(".tags_clear")));
+    let tabs = await driver.findElement(swd.By.css("li[data-tab=moderators]"));
+    await tabs.click();
+    let moderator = await driver.findElement(swd.By.css("#moderator"));
+    await moderator.sendKeys(userToAdd);
+    await moderator.sendKeys(swd.Key.ENTER);
+    let saveBtn = await driver.findElement(swd.By.css(".save-challenge.btn.btn-green"));
+    await saveBtn.click();
 }
